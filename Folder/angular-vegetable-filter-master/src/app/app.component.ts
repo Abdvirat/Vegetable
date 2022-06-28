f31b5ac5063190d0e74c4429e10329cc35246e6c
Shivam singh <88611640+Abdvirat@users.noreply.github.com>
2022-06-28 16:45:50 +0530

    Delete Folder/angular-vegetable-filter-master/src directory

--- Folder/angular-vegetable-filter-master/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { SearchComponent } from './search/search.component';
-import { ListComponent } from './list/list.component';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {}
